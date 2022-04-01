de6c6e4c6edfdcfe361c79861951d0f25e01f31e
Daniela Brenes <danibreo6@gmail.com>
2022-03-31 20:39:38 -0600

    Model scaffolding

---
+++ API/TECAirDbAPI/Models/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TECAirDbAPI.Models
+{
+    public partial class Plane
+    {
+        /*public Plane()
+        {
+            Flights = new HashSet<Flight>();
+        }*/
+
+        public string Planeid { get; set; }
+        public string Model { get; set; }
+        public int Passengercap { get; set; }
+        public int Bagcap { get; set; }
+
+        //public virtual ICollection<Flight> Flights { get; set; }
+    }
+}
--- API/TECAirDbAPI/Startup.cs
+++ API/TECAirDbAPI/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,7 @@ namespace TECAirDbAPI
         public void ConfigureServices(IServiceCollection services)
         {
 
+            services.AddDbContext<TECAirDbContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
