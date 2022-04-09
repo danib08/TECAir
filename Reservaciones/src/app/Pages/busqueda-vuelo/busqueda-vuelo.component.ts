873379a4cd1897a97b5d339e90491ca7f88b9f32
Aldo Cambronero Ureña <44718310+Daval03@users.noreply.github.com>
2022-04-08 20:36:13 -0600

    Implementacion de busqueda y reservacion de vuelos

--- Reservaciones/src/app/Pages/busqueda-vuelo/busqueda-vuelo.component.ts
+++ Reservaciones/src/app/Pages/busqueda-vuelo/busqueda-vuelo.component.ts
@@ -1,4 +1,79 @@
 import { Component, OnInit } from '@angular/core';
+import { Vuelos } from '../models/vuelos.model';
+
+// Test for displaying all the flights
+const COUNTRIES: Vuelos[] = [
+  {
+    Origin: "Mexico",
+    Destination: "United States",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:450
+  },
+  {
+    Origin: "Canada",
+    Destination: "United States",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:150
+  },
+  {
+    Origin: "Costa Rica",
+    Destination: "Canada",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:400
+  },
+  {
+    Origin: "Panama",
+    Destination: "España",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:350
+  },
+  {
+    Origin: "Japon",
+    Destination: "España",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:550
+  },
+  {
+    Origin: "Peru",
+    Destination: "Costa Rica",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime: "21:00",
+    ArrivalTime:"12:47",
+    Price:430
+  },
+  {
+    Origin: "Uruguay",
+    Destination: "Russia",
+    BagQuantity: 4546,
+    UserQuantity: 8994,
+    FlightID: "XML5214",
+    DepartureTime:"21:00",
+    ArrivalTime: "12:47",
+    Price:40
+  }
+];
 
 @Component({
   selector: 'app-busqueda-vuelo',
@@ -9,7 +84,21 @@ export class BusquedaVueloComponent implements OnInit {
 
   constructor() { }
 
+  Estado=false;
+
+  //List with all the flights
+  listVuelos= COUNTRIES;
   ngOnInit(): void {
   }
+  //Testing the button
+  mostrarInfo(event: { preventDefault: () => void; target: any; }){
+    event.preventDefault()
+    const target= event.target
+    console.log("Hola mundo")
+    this.Estado=true;
+  }
+  getListaVuelos(event: { preventDefault: () => void; target: any; }){
+    //Ask API for all the flight that comes form Origin to Destination
 
+  }
 }
--- Reservaciones/src/app/Pages/reservacion-vuelos/reservacion-vuelos.component.ts
+++ Reservaciones/src/app/Pages/reservacion-vuelos/reservacion-vuelos.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReservacionVuelosComponent implements OnInit {
 
+  nombreUsuario:string="Aldo Cambronero";
+  gate:number=45;
+  DateTimeDeparture:string="";
+  DateTimeArrival:string="";
+  Origin:string="CR";
+  Destination:string="MDR";
+  Stops:string="---";
+  Price:number=0;
+  Discount:number=0;
+  Flightid:string="XSQ124";
   constructor() { }
 
   ngOnInit(): void {
